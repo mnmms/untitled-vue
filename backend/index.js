054f70ba47c612d31660789a82538581f89c72a0
이소정 <esomnmm@gmail.com>
2024-01-05 12:40:03 +0900

    event 발생 순서

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,13 @@ const port = 3000;
 const memos = ["메모1", "메모2", "메모3", "메모4", "메모5", "메모6", "메모7", "메모8"];
 
 // respond with "hello world" when a GET request is made to the homepage
-app.get("/api/memos", (req, res) => {
-  res.status(202).send(memos);
+// app.get("/api/memos", (req, res) => {
+//   res.status(202).send(memos);
+// });
+
+app.get("/api/memos", async (req, res) => {
+  // const result = await database.run("SELECT * FROM memos");
+  res.send(memos);
 });
 
 // app.get("/api", (req, res) => {
@@ -16,6 +21,10 @@ app.get("/api/memos", (req, res) => {
 //   res.status(202).send(memos);
 // });
 
+// app.listen(port, () => {
+//   console.log(`connected ${port} server`);
+// });
+
 app.listen(port, () => {
-  console.log(`connected ${port} server`);
+  console.log(`Example app listening at http://localhost:${port}`);
 });
